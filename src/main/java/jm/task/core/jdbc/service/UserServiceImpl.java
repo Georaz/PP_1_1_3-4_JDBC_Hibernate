ceea5192abd0a2a21d095124acd667351dbc5777
Georaz <lizard0930@mail.ru>
2023-08-27 13:18:41 +0300

    Adds override annotation

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -11,27 +11,33 @@ public class UserServiceImpl implements UserService {
     public UserServiceImpl() {
     }
 
+    @Override
     public void createUsersTable() {
         userDao.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         userDao.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
         System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
+    @Override
     public void removeUserById(long id) {
         userDao.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
     }
